9e75d81d19eda9833947c9da6476620b3c8f6d0e
s20699 <s20699@pjwstk.edu.pl>
2022-06-19 01:38:07 +0200

    Dodano asocjacje kwalifikowaną Boss - Shop by id

--- MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/Boss.java
+++ MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/Boss.java
@@ -1,8 +1,29 @@
 package ekomp.Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Boss extends ObjectPlus{
     private int id;
+    private List<Shop> shops = new ArrayList<>();
+
     public Boss(int id) {
         this.id = id;
+    }
+
+    // Getery i seter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void addShop(Shop shop) {
+        if (!shops.contains(shop)) {
+            shops.add(shop);
+            shop.addBossQualif(this);
+        }
     }
 }
--- MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/Shop.java
+++ MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/Shop.java
@@ -2,15 +2,24 @@ package ekomp.Models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Shop extends ObjectPlus {
     private String shopName;
     private Adress adress;
     private List<Employee> employees = new ArrayList<>();
+    private Map<Integer,Boss> bossQualif = new TreeMap<>();
 
     public Shop(String shopName, Adress adress) {
         this.shopName = shopName;
         this.adress = adress;
+    }
+
+    public Boss findBossQualif(int id) throws Exception {
+        if (!bossQualif.containsKey(id))
+            throw new Exception("Nie znaleziono Szefa o takim id");
+        return bossQualif.get(id);
     }
 
     // Realizacja asocjacji
@@ -24,6 +33,12 @@ public class Shop extends ObjectPlus {
     public void removeEmployee(Employee employee) {
         if(employees.contains(employee)) {
             employees.remove(employee);
+        }
+    }
+    public void addBossQualif(Boss boss) {
+        if (!bossQualif.containsKey(boss.getId())) {
+            bossQualif.put(boss.getId(), boss);
+            boss.addShop(this);
         }
     }
 }
