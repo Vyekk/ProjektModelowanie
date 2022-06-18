281ba649508445a2d438af2e3493f154bbf6ff9d
s20699 <s20699@pjwstk.edu.pl>
2022-06-18 21:16:52 +0200

    Dodano potrzebne kolekcje

--- MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/ObjectPlusPlus.java
+++ MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Models/ObjectPlusPlus.java
@@ -1,5 +1,15 @@
 package ekomp.Models;
 
-public class ObjectPlusPlus extends ObjectPlus{
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Set;
 
+public abstract class ObjectPlusPlus extends ObjectPlus{
+    private Map<String,Map<Object,ObjectPlusPlus>> links = new Hashtable<>();
+    private static Set<ObjectPlusPlus> allParts = new HashSet<>();
+
+    public ObjectPlusPlus() {
+        super();
+    }
 }
