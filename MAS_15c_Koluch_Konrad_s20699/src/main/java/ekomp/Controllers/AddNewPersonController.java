2fb4f18ae02dbc6d60f5abb4ea50cb8b0186f0a2
s20699 <s20699@pjwstk.edu.pl>
2022-06-20 04:15:23 +0200

    Wprowadzono pierwsze ekrany i metody gui

---
+++ MAS_15c_Koluch_Konrad_s20699/src/main/java/ekomp/Controllers/AddNewPersonController.java
@@ -0,0 +1,19 @@
+package ekomp.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.RadioButton;
+
+public class AddNewPersonController {
+    @FXML
+    RadioButton rServiceTechnician, rVendor;
+    @FXML
+    CheckBox cClient, cBoss;
+
+    @FXML
+    public void showDateSalary() {
+
+    }
+    @FXML
+    public void showId() {}
+}
